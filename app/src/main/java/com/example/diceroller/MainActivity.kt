a45ed1c69aebbd9e82dd496ca92b35def3d3df59
quang1810 <n.trongquang2002@gmail.com>
2023-10-08 12:15:11 +0700

    them hinh anh

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,13 +13,24 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+        rollDice()
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val rollDice = dice.roll()
-        var t: TextView = findViewById(R.id.textView)
-        t.text = rollDice.toString()
+        var diceImage: ImageView = findViewById(R.id.imageView)
+
+        val drawableResource = when (rollDice) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = rollDice.toString()
     }
 }
 
